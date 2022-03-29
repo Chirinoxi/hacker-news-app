a3d95c1943c83b52e67740dadf067832dba3e4cb
Ignacio Chirino <ignacio.chirino@alumnos.ucn.cl>
2022-03-29 12:49:09 -0300

    feat(app): added typing to the API response and news service was created.

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hit, NewsResponse } from '../interfaces/news-response.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NewsService {
+  private baseUrl: string = 'http://hn.algolia.com/api/v1';
+  private _hits: Hit[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public get hits(): Hit[] {
+    return this._hits;
+  }
+
+  public set hits(value: Hit[]) {
+    this._hits = value;
+  }
+
+  public addParameterToUrl(url: URL, key: string, value: string): URL {
+    let _url = url;
+    key = encodeURIComponent(key);
+    value = encodeURIComponent(value);
+    _url.searchParams.append(key, value);
+    return _url;
+  }
+
+  public filterData(hit: Hit): boolean {
+    if (
+      hit.author != null &&
+      hit.story_title != null &&
+      hit.story_url != null &&
+      hit.created_at != null
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  public obtainHackerNews(
+    query: string,
+    page: number
+  ): Observable<NewsResponse> {
+    let url = new URL(this.baseUrl + '/search_by_date?');
+    url = this.addParameterToUrl(url, 'query', query);
+    url = this.addParameterToUrl(url, 'page', page.toString());
+    url = this.addParameterToUrl(url, 'hitsPerPage', '150');
+    return this.http.get<NewsResponse>(url.toString());
+  }
 }
